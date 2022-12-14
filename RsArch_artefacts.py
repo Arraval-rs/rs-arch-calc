52bf11b56cef12b50b536a92a80405eaa4d8601a
Cody Coombs <cody.coombs@live.ca>
2022-12-14 17:26:55 -0330

    Condensed artefact tab creation

--- RsArch_artefacts.py
+++ RsArch_artefacts.py
@@ -9,49 +9,24 @@ import json
 import PySimpleGUI as sg
 import RsArch_functions as RsA_f
 
-# Create the artefact layout given the list of images for a faction
-def create_artefact_frame(images):
+# Create the artefact layout given a faction
+def create_artefact_frame(faction):
 	frame = [[]]
-	for i in range(0, len(images), 6):
+	for i in range(0, len(RsA_f.read_value(RsA_f.arch_dict,["Artefacts", faction])), 5):
 		frame.append([])
 		frame.append([])
-		for j in range(0, min(6, len(images[i:i+6]))):
-			frame[int(2*i/6)].append(sg.Image(images[i+j]))
-			frame[int(2*i/6+1)].append(sg.Input(default_text = "0", enable_events = True, justification = "right", size = (4, 1))) # key = "{}Artefacts_{}".format(i+j)
+		for j in range(0, min(5, len(RsA_f.read_value(RsA_f.arch_dict,["Artefacts",faction])[i:i+5]))):
+			frame[int(2*i/5)].append(sg.Image(RsA_f.generate_img("images/artefacts/{}".format(RsA_f.read_value(RsA_f.arch_dict,["Artefacts",faction,i+j,"name"])+" (damaged).png"), (31, 31), True)))
+			frame[int(2*i/5+1)].append(sg.Input(default_text = "0", enable_events = True, justification = "right", size = (4, 1))) # key = "{}Artefacts_{}".format(i+j)
 	return frame
-
-# Creating arrays of images
-armadyl_images = []
-for i in range(len(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Armadylean"]))):
-	armadyl_images.append(RsA_f.generate_img("images/artefacts/{}".format(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Armadylean",i,"name"])+" (damaged).png"), (31, 31), True))
-
-bandos_images = []
-for i in range(len(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Bandosian"]))):
-	bandos_images.append(RsA_f.generate_img("images/artefacts/{}".format(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Bandosian",i,"name"])+" (damaged).png"), (31, 31), True))
-
-dragonkin_images = []
-for i in range(len(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Dragonkin"]))):
-	dragonkin_images.append(RsA_f.generate_img("images/artefacts/{}".format(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Dragonkin",i,"name"])+" (damaged).png"), (31, 31), True))
-
-saradomin_images = []
-for i in range(len(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Saradominist"]))):
-	saradomin_images.append(RsA_f.generate_img("images/artefacts/{}".format(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Saradominist",i,"name"])+" (damaged).png"), (31, 31), True))
-
-zamorak_images = []
-for i in range(len(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Zamorakian"]))):
-	zamorak_images.append(RsA_f.generate_img("images/artefacts/{}".format(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Zamorakian",i,"name"])+" (damaged).png"), (31, 31), True))
-
-zaros_images = []
-for i in range(len(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Zarosian"]))):
-	zaros_images.append(RsA_f.generate_img("images/artefacts/{}".format(RsA_f.read_value(RsA_f.arch_dict,["Artefacts","Zarosian",i,"name"])+" (damaged).png"), (31, 31), True))
-
+	
 # Creating layouts for each faction's artefacts
-armadyl_frame = create_artefact_frame(armadyl_images)
-bandos_frame = create_artefact_frame(bandos_images)
-dragonkin_frame = create_artefact_frame(dragonkin_images)
-saradomin_frame = create_artefact_frame(saradomin_images)
-zamorak_frame = create_artefact_frame(zamorak_images)
-zaros_frame = create_artefact_frame(zaros_images)
+armadyl_frame = create_artefact_frame("Armadylean")
+bandos_frame = create_artefact_frame("Bandosian")
+dragonkin_frame = create_artefact_frame("Dragonkin")
+saradomin_frame = create_artefact_frame("Saradominist")
+zamorak_frame = create_artefact_frame("Zamorakian")
+zaros_frame = create_artefact_frame("Zarosian")
 
 artefacts_tab = [[
 					sg.Column(
